f6779d89cf9b186a18bb59eaa56763f57d681ac7
emmagc00 <72264467+emmagc00@users.noreply.github.com>
2021-05-28 16:28:00 +0200

    repository
    fatte le repository di base e fatta senza problemi la run il problema era uno spazio di troppo nell'applicationproperties

---
+++ siw-museo/src/main/java/it/uniroma3/siw/spring/repository/ArtistaRepository.java
@@ -0,0 +1,22 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Artista;
+
+public interface ArtistaRepository extends CrudRepository<Artista, Long>{
+
+	public List<Artista> findByNome(String nome);
+
+	public List<Artista> findByNomeAndCognome(String nome, String cognome);
+
+	public List<Artista> findByNomeOrCognome(String nome, String cognome);
+	
+	public Optional<Artista> findById(Long id);
+	
+	public List<Artista> findAll();
+
+}
---
+++ siw-museo/src/main/java/it/uniroma3/siw/spring/repository/OperaRepository.java
@@ -0,0 +1,18 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Opera;
+
+public interface OperaRepository extends CrudRepository<Opera, Long> {
+
+	public List<Opera> findByTitolo(String titolo);
+
+	public Optional<Opera> findById(Long id);
+	
+	public List<Opera> findAll();
+
+}
