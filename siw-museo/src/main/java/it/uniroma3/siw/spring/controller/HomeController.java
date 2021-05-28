e966964f7eab41e49ae9fe3a3ff866d9f72e2ec7
emmagc00 <72264467+emmagc00@users.noreply.github.com>
2021-05-28 16:24:51 +0200

    fatte le repository di base per tutte le classi

--- siw-museo/src/main/java/it/uniroma3/siw/spring/controller/HomeController.java
+++ siw-museo/src/main/java/it/uniroma3/siw/spring/controller/HomeController.java
@@ -1,13 +1,13 @@
-package it.uniroma3.siw.spring.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-public class HomeController{
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String prova() {
-		return "Collezioni.html";
-	}
-}
+//package it.uniroma3.siw.spring.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@Controller
+//public class HomeController{
+//	@RequestMapping(value="/collezioni", method=RequestMethod.GET)
+//	public String prova() {
+//		return "Collezioni.html";
+//	}
+//}
---
+++ siw-museo/src/main/java/it/uniroma3/siw/spring/repository/CollezioneRepository.java
@@ -0,0 +1,18 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Collezione;
+import it.uniroma3.siw.spring.model.Curatore;
+
+public interface CollezioneRepository extends CrudRepository<Collezione, Long> {
+
+	public List<Collezione> findByNome(String nome);
+
+	public List<Collezione> findByCuratore(Curatore curatore);
+
+	public Optional<Collezione> findById(Long id);
+}
